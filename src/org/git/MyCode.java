c9558076cc78d07c706730fd6499dafe631ed3fc
PrakashRK17 <videomemes17@gmail.com>
2022-04-26 23:11:08 +0530

    Class

---
+++ src/org/git/MyCode.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class MyCode {
+
+	private void tc1() {
+		System.out.println("Tc1 Completed");
+	}
+public static void main(String[] args) {
+	MyCode m = new MyCode();
+	m.tc1();
+}
+}
